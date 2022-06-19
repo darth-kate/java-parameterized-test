e8b1df1c16ae4dee8a2d62a8062a5f37b59c74ea
Ekaterina Antonova <kate.antonova1209@gmail.com>
2022-06-19 13:51:00 +0300

    Added parameterized test for checkIsAdult

---
+++ src/main/java/checkIsadult.java
@@ -0,0 +1,10 @@
+class Program {
+    boolean result;
+    public boolean checkIsAdult(int age) {
+        return result = 18 <= age;
+        }
+
+    }
+
+
+
---
+++ src/test/java/CheckIsAdultTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                {21, true},
+                {18, true},
+                {17, false},
+                {19, true}
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        boolean isAdult = program.checkIsAdult(age);
+
+        assertEquals("Должно возвращать true для совершеннолетних, false - для несовершеннолетних", result, isAdult);
+    }
+}
+
